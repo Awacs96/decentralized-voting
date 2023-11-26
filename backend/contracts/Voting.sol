b76b2dc7e91c99edb4a7587c5fe124e367e1ed87
Awacs <tomas.kosub@gmail.com>
2023-11-26 16:01:13 +0100

    finished contract

--- backend/contracts/Voting.sol
+++ backend/contracts/Voting.sol
@@ -25,5 +25,49 @@ contract Voting {
         _;
     }
 
+    modifier canVote(uint voteId, uint option) {
+        require(voteId < nextVoteId, "The vote does not exist.");
+        require(option < votes[voteId].options, "Invalid option.");
+        require(!votes[voteId].voted[msg.sender], "You have already voted.");
+        require(votes[voteId].endTime >= block.timestamp, "The vote has ended.");
+        _;
+    }
+
+    function join() external {
+        require(!isMember[msg.sender], "You are a member already.");
+
+        isMember[msg.sender] = true;
+        emit MemberJoined(msg.sender, block.timestamp);
+    }
+
+    function createVote(string memory uri, uint endTime, uint options) external senderIsMember {
+        require(options >= 2 && options <= 5, "Number of options can be between 2 and 5.");
+        require(endTime > block.timestamp, "The endTime cannot be in the past.");
+
+        uint voteId = nextVoteId;
+        votes[voteId].uri = uri;
+        votes[voteId].owner = msg.sender;
+        votes[voteId].endTime = endTime;
+        votes[voteId].votes = new uint[](options);
+        votes[voteId].options = options;
+
+        emit VoteCreated(msg.sender, voteId, block.timestamp, endTime);
+        nextVoteId++;
+    }
+
+    function vote(uint voteId, uint option) external senderIsMember canVote(voteId, option) {
+        votes[voteId].voted[msg.sender] = true;
+        votes[voteId].votes[option]++;
+
+        emit Voted(msg.sender, voteId, option, block.timestamp);
+    }
+
+    function getVote(uint voteId) public view returns(string memory, address, uint[] memory, uint) {
+        return (votes[voteId].uri, votes[voteId].owner, votes[voteId].votes, votes[voteId].endTime);
+    }
+
+    function didVote(address member, uint voteId) public view returns(bool) {
+        return votes[voteId].voted[member];
+    }
 
 }
