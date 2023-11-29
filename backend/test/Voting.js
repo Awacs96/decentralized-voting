3a954e40b779eb6b980820e6831bcbd3f7be8c4e
Tomas Kosub <tomas.kosub@gmail.com>
2023-11-29 06:06:49 +0100

    finished testing suite

--- backend/test/Voting.js
+++ backend/test/Voting.js
@@ -33,6 +33,7 @@ describe("Voting", function () {
       await expect(votingContract.connect(addr1).createVote("myRandomUri", (await getTime() + 60), 3)).to.be.reverted;
     });
 
+    // VoteId = 0
     it("Member can create vote", async () => {
       await expect(votingContract.createVote("myRandomUri", (await getTime() + 60), 5)).to.emit(votingContract, "VoteCreated");
     });
@@ -49,10 +50,38 @@ describe("Voting", function () {
       await expect(votingContract.createVote("myRandomUri", (await getTime() + 30), 1)).to.be.reverted;
     });
 
-    // Take a deeper look -> 2 votes with same URI - should be allowed or not?
+    // VodeId = 1; Take a deeper look -> 2 votes with same URI - should be allowed or not?
     it("Member can create a second vote", async () => {
       await expect(votingContract.createVote("myRandomUri", (await getTime() + 30), 4)).to.emit(votingContract, "VoteCreated");
     });
   });
 
+  describe("Vote", () => {
+    it("Can not vote if the sender is not a member", async () => {
+      await expect(votingContract.connect(addr1).vote(0, 2)).to.be.reverted;
+    });
+
+    // We created 2 votes (Id 0 and 1)
+    it("Can not vote in a vote that does not exist", async () => {
+      await expect(votingContract.vote(2, 2)).to.be.reverted;
+    });
+
+    it("Can not vote for an invalid option", async () => {
+      await expect(votingContract.vote(1, 4)).to.be.reverted;
+    });
+
+    it("Member can vote", async () => {
+      await expect(votingContract.vote(1, 2)).to.emit(votingContract, "Voted");
+    });
+
+    it("Member can not vote twice in the same vote", async () => {
+      await expect(votingContract.vote(1, 3)).to.be.reverted;
+    });
+
+    it("Member can not vote on an expired vote", async () => {
+      await votingContract.connect(addr1).join();
+      await ethers.provider.send("evm_mine", [(await getTime()) + 3600]);
+      await expect(votingContract.connect(addr1).vote(1, 2)).to.be.reverted;
+    });
+  });
 });
